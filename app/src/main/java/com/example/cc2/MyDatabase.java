20dad5da2f2833d88e5baad7cb1a1301e26463a2
OmarR19 <bnlkddomar399@outlook.fr>
2022-06-02 14:20:17 +0100

    Question4

--- app/src/main/java/com/example/cc2/MyDatabase.java
+++ app/src/main/java/com/example/cc2/MyDatabase.java
@@ -8,6 +8,12 @@ import androidx.annotation.Nullable;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
+    public static String DB_NAME="Entreprise.db";
+    public static String TABLE_NAME="Entreprise";
+    public static int COL1= Integer.parseInt("Id");
+    public static String COL2="RaisonSoc";
+    public static String COL3="Adresse";
+    public static double COL4= Double.parseDouble("Capital");
 
 
     public MyDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
